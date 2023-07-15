b9329894e3bcc0a0e76069552b729f7fb0a80aeb
salami kabiru <kabirusalami41@gmail.com>
2023-07-15 12:47:11 +0100

    beautiful code

--- beautiful.py
+++ beautiful.py
@@ -1,4 +1,4 @@
+#!/usr/bin/python3
 """ Beautiful code that passes Pyscodestyle """
-
 
 print("Hello World")
--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Entry point of the command interpreter """
 
+
 import cmd
 from models import storage
 from models.base_model import BaseModel
